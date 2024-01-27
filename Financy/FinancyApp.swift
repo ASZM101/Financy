3157df76bb73c9b3f9abc47f63de8568c9994c03
ASZM101 <adrianasofia.zambrano@gmail.com>
2024-01-27 12:40:24 -0600

    Initial Commit

---
+++ Financy/FinancyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancyApp.swift
+//  Financy
+//
+//  Created by Adriana Zambrano on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FinancyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
