edd92698a72e78f982210ec0d2c6edea682e84a5
ASZM101 <adrianasofia.zambrano@gmail.com>
2024-01-27 14:02:05 -0600

    Created home page

---
+++ Financy/NewEntry.swift
@@ -0,0 +1,18 @@
+//
+//  NewEntry.swift
+//  Financy
+//
+//  Created by Adriana Zambrano on 1/27/24.
+//
+
+import SwiftUI
+
+struct NewEntry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewEntry()
+}
